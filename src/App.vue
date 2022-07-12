effcde62fc208cd4b671ed407353263799ef279a
Indra Rudianto <indrarudianto.official@gmail.com>
2022-07-12 12:38:05 +0700

    Update app layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark> </v-app-bar>
-    <v-main>
-      <router-view />
-    </v-main>
+    <v-app-bar app color="white" flat outlined>
+      <v-app-bar-nav-icon />
+      <v-toolbar-title>Chart Studio</v-toolbar-title>
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
   </v-app>
 </template>
 
