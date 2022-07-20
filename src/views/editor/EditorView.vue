98df500a15daae70d787bcf1a27dbc29c6cbf9a0
Indra Rudianto <indrarudianto.official@gmail.com>
2022-07-20 10:46:48 +0700

    Save working code

--- src/views/editor/EditorView.vue
+++ src/views/editor/EditorView.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>EditorView</div>
+  <v-app>
+    <v-app-bar app flat dense>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-main></v-main>
+  </v-app>
 </template>
