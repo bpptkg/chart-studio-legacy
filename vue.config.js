0b0e096c4fd18e20d93408ef1ddd5cbcb4362a3d
Indra Rudianto <indrarudianto.official@gmail.com>
2022-08-05 11:26:41 +0700

    Update public path config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: ['vuetify'],
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? process.env.VUE_APP_PUBLIC_PATH || '/chart-studio/'
+      : '/',
 });
