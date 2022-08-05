0c9e4b376fb460049364aac38231b6a3747344ee
Indra Rudianto <indrarudianto.official@gmail.com>
2022-08-05 12:35:22 +0700

    Fetch user on load

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import axios from 'axios';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
@@ -7,9 +8,30 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  pinia,
-  vuetify,
-  render: (h) => h(App),
-}).$mount('#app');
+function initApp() {
+  new Vue({
+    router,
+    pinia,
+    vuetify,
+    render: (h) => h(App),
+  }).$mount('#app');
+}
+
+async function initialize() {
+  axios
+    .get('/user')
+    .then(() => {
+      initApp();
+    })
+    .catch(() => {
+      if (process.env.NODE_ENV === 'production') {
+        return (window.location.href = process.env.VUE_APP_CENDANA_URL || '/');
+      } else {
+        initApp();
+      }
+    });
+}
+
+window.addEventListener('load', () => {
+  initialize();
+});
