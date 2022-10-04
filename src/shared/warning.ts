2bdc832b16b89927da4c29d4beee26fbfb3ae3bc
Indra Rudianto <indrarudianto.official@gmail.com>
2022-10-04 12:39:09 +0700

    Run linting

--- src/shared/warning.ts
+++ src/shared/warning.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function warn(msg: string, ...args: any[]): void {
   console.warn(`[ChartStudio warn] ${msg}`, ...args)
 }
--- src/shims-splitpanes.d.ts
+++ src/shims-splitpanes.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'splitpanes' {
   let Splitpanes: any
   let Pane: any
